e358e0c677cfbcd5326a47e0c3ce66019c3ba018
Enzo Hideki Yoshida <102935194+ewzxyh@users.noreply.github.com>
2023-10-10 20:55:19 -0300

    Correção Preço de Custo Pagina.tsx
    Corrigido adicionar preço de custo, valor agora é atualizado, e ao remover o valor não fica negativo.
    
    Mudança: adicionado a linha restaurante.total = novoCusto; antes da chamada fetch.

--- src/components/pages/Pagina.tsx
+++ src/components/pages/Pagina.tsx
@@ -1,14 +1,14 @@
 import { useParams } from "react-router-dom";
-import styles from "./Pagina.module.css";
-import {useState, useEffect} from "react";
-import {v4 as uuidv4} from "uuid";
+import styles from "./PaginaEdicao.module.css";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Mensagem from "../layout/Mensagem";
 import Container from "../layout/Container";
-import ItemForm from "../itens/ItemForm";
-import IngredientesForm from "../itens/IngredientesForm";
-import ValorCard from "../itens/ValorCard";
+import ItemForm from "../Itens/ItemForm";
+import IngredientesForm from "../Itens/IngredientsForm";
+import ValorCard from "../Itens/ValorCard";
 
-interface TipoType{
+interface TipoType {
     id: number;
     name: string;
 }
@@ -22,122 +22,124 @@ interface ValorType {
 
 interface RestauranteData {
     id: number;
-    name:string;
+    name: string;
     price: number;
     tipos: TipoType;
     total: number;
     valores: ValorType[];
 }
 
-function Pagina (){
-    const {id} = useParams();
+function PaginaEdicao() {
+    const { id } = useParams<{ id: string }>();
     const [restaurante, setRestaurante] = useState<RestauranteData>();
     const [showItemForm, setShowItemForm] = useState(false);
     const [showServiceForm, setShowServiceForm] = useState(false);
     const [mensagem, setMensagem] = useState("");
     const [type, setType] = useState("");
     const [valores, setValores] = useState<ValorType[]>([]);
-    
-    useEffect ( () =>{
-        fetch(`http://localhost:8080/restaurante/${id}`,{
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json"
+    useEffect(() => {
+        fetch(`http://localhost:8080/restaurante/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
             }
         })
-        .then((resp)=>resp.json())
-        .then((dados) =>{
-            setRestaurante(dados);
-            setValores(dados.valores);
-        })
-        .catch((e)=>console.log(e));
-    },[id]);
-
-    function editPost(res:RestauranteData) {
+            .then((resp) => resp.json())
+            .then((dados) => {
+                setRestaurante(dados);
+                setValores(dados.valores);
+            })
+            .catch((e) => console.log(e));
+    }, [id]);
+    function editPost(res: RestauranteData) {
         setMensagem("");
-        if(res.price < res.total){
+        if (res.price < res.total) {
             setMensagem("O preço de custo não pode ser menor que o preço do Item");
             setType("erro");
             return false;
         }
 
-        fetch (`http://localhost:8080/restaurante/${res.id}`,{
-            method:"PATCH",
-            headers:{"Content-Type":"application/json"},
+        fetch(`http://localhost:8080/restaurante/${res.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(res)
         })
-        .then((res) => res.json())
-        .then((data) => {
-            setRestaurante(data);
-            setShowItemForm(false);
-            setMensagem("Item Atualizado!");
-            setType("sucesso");
-        })
-        .catch((erro) => console.log(erro));
+            .then((res) => res.json())
+            .then((data) => {
+                setRestaurante(data);
+                setShowItemForm(false);
+                setMensagem("Item Atualizado!");
+                setType("sucesso");
+            })
+            .catch((erro) => console.log(erro));
     }
 
-    function createIngrediente(restaurante:RestauranteData) {
+    function createIngrediente(restaurante: RestauranteData) {
         setMensagem("");
         const ultimoCusto = restaurante.valores[restaurante.valores.length - 1];
         ultimoCusto.id = uuidv4();
         const novoCusto = Number(restaurante.total) + Number(ultimoCusto.custo);
-
-        if (novoCusto > Number(restaurante.price)){
+    
+        if (novoCusto > Number(restaurante.price)) {
             setMensagem("Custo total maior que o preço de venda!");
             setType("erro");
             restaurante.valores.pop();
             return false;
         }
-        fetch (`http://localhost:8080/restaurante/${restaurante.id}`,{
-            method:"PATCH",
-            headers:{"Content-Type":"application/json"},
+        
+        restaurante.total = novoCusto; // Adicione esta linha
+    
+        fetch(`http://localhost:8080/restaurante/${restaurante.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(restaurante)
         })
-        .then((res) => res.json())
-        .then(() => {
-            setShowServiceForm(false);
-            setMensagem("Valor Atualizado!");
-            setType("sucesso");
-        })
-        .catch((erro) => console.log(erro));
+            .then((res) => res.json())
+            .then(() => {
+                setShowServiceForm(false);
+                setMensagem("Valor Atualizado!");
+                setType("sucesso");
+            })
+            .catch((erro) => console.log(erro));
     }
     
-    function removerCusto(id:string, total:number){
+
+    function removerCusto(id: string, total: number) {
         setMensagem("");
-        if(!restaurante) return;
-        const atualizaCusto = 
-            restaurante.valores.filter((valor) =>valor.id !== id);
+        if (!restaurante) return;
+        const atualizaCusto =
+            restaurante.valores.filter((valor) => valor.id !== id);
         const atualizaRestaurante = restaurante;
         atualizaRestaurante.valores = atualizaCusto;
-        atualizaRestaurante.total = 
+        atualizaRestaurante.total =
             Number(atualizaRestaurante?.total) - Number(total);
-        fetch (`http://localhost:8080/restaurante/${restaurante.id}`,{
-            method:"PATCH",
-            headers:{"Content-Type":"application/json"},
+        fetch(`http://localhost:8080/restaurante/${restaurante.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(atualizaRestaurante)
         })
-        .then((res) => res.json())
-        .then(() => {
-            setRestaurante(atualizaRestaurante);
-            setValores(atualizaCusto);
-            setMensagem("Valor Removido com Sucecesso!");
-            setType("sucesso");
-        })
-        .catch((erro) => console.log(erro));
+            .then((res) => res.json())
+            .then(() => {
+                setRestaurante(atualizaRestaurante);
+                setValores(atualizaCusto);
+                setMensagem("Valor Removido com Sucecesso!");
+                setType("sucesso");
+            })
+            .catch((erro) => console.log(erro));
     }
 
     function toggleItemForm() {
-        setShowItemForm (!showItemForm);
+        setShowItemForm(!showItemForm);
     }
 
-    function toggleServiceForm(){
+    function toggleServiceForm() {
         setShowServiceForm(!showServiceForm);
     }
 
     return (
         <>
             {restaurante?.name ? (
-                <div className = {styles.pagina_details}>
+                <div className={styles.pagina_details}>
                     {mensagem && <Mensagem msg={mensagem} type={type} />}
                     <Container customClass="column">
                         <div className={styles.details_container}>
@@ -152,67 +154,64 @@ function Pagina (){
                                     </p>
                                     <p>
                                         <span>Preço do Item: </span> R${" "}
-                                        {(restaurante.price-0).toFixed(2)}
+                                        {(restaurante.price - 0).toFixed(2)}
                                     </p>
                                     <p>
                                         <span>Preço de Custo: </span> R${" "}
-                                        {(restaurante.total-0).toFixed(2)}
+                                        {(restaurante.total - 0).toFixed(2)}
                                     </p>
                                     <p>
                                         <span>Lucro: </span>R${" "}
                                         {(restaurante.price - restaurante.total).toFixed(2)}
                                     </p>
                                 </div>
-                            ): 
-                            (
-                                <div className = {styles.pagina_info}>
-                                    <ItemForm handleSubmit={editPost}
-                                        btnText="Concluir Edição"
-                                        restauranteData={restaurante} />
-                                </div>
-                            )}
+                            ) :
+                                (
+                                    <div className={styles.pagina_info}>
+                                        <ItemForm handleSubmit={editPost}
+                                            btnText="Concluir Edição"
+                                            restauranteData={restaurante} />
+                                    </div>
+                                )}
                         </div>
                         <div className={styles.form_container}>
-                            <h2>Adicionar Custo:</h2>
+                            <h2>Adicionar Custo</h2>
                             <button className={styles.btn} onClick={toggleServiceForm}>
                                 {!showServiceForm ? "Adicionar Custos" : "Fechar"}
                             </button>
                             <div className={styles.pagina_info}>
                                 {showServiceForm && (
                                     <div>
-                                        <IngredientesForm 
+                                        <IngredientesForm
                                             handleSubmit={createIngrediente}
                                             btnText="Adicionar Custo"
-                                            itemData={restaurante} />
-
+                                            itemData={restaurante}
+                                        />
                                     </div>
                                 )}
                             </div>
                         </div>
                         <h2>Custos</h2>
                         <Container customClass="start">
-                            {valores.length > 0 &&
-                            valores.map((valor: ValorType) =>(
+                            {valores.length > 0 && valores.map((valor: ValorType) => (
                                 <ValorCard
                                     id={valor.id}
                                     name={valor.name}
                                     custo={valor.custo}
                                     key={valor.id}
                                     handleRemove={removerCusto}
-                                    />
-                            ))
-                            }
-                        {valores.length === 0 && <p>Não há valores cadastrados!</p>}
+                                />
+                            ))}
+                            {valores.length === 0 && <p>Não há valores cadastrados</p>}
                         </Container>
                     </Container>
                 </div>
-            ):(
-                <div className={styles.pagina_details}>
-                    <h1>Nada por aqui ainda!</h1>
-                </div>
-            )}
+            ) : () => <div className={styles.pagina_details}>
+                <h1>Nada por aqui ainda!</h1>
+            </div>
+            }
         </>
-    );
+    )
 }
 
-export default Pagina;
+export default PaginaEdicao;
